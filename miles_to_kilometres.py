71729014d10027d45a847a8b2ab02434947bee19
Appsie <appsie095@hotmail.com>
2016-05-13 11:27:46 +1000

    finished the m to km part of workshop

--- miles_to_kilometres.py
+++ miles_to_kilometres.py
@@ -7,7 +7,22 @@ class milestokilometers(App):
         self.root = Builder.load_file('miles_to_kilometres.kv')
         return self.root
 
+    def calculate(self):
+        miles = self.error_check_miles()
+        kilometers = miles * 1.60934
+        self.root.ids.output_label.text = str(kilometers)
 
+    def handle_increment(self, increment):
+        miles = self.error_check_miles() + increment
+        self.root.ids.input_number.text = str(miles)
+        self.calculate()
+
+    def error_check_miles(self):
+        try:
+            miles = float(self.root.ids.input_number.text)
+            return miles
+        except ValueError:
+            return 0
 
 
 
